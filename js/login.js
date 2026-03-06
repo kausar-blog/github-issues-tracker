9bc3200e6232232708c88c11627cab37bd199be4
kausar-blog <kausarprogrammer@gmail.com>
2026-03-07 00:33:26 +0600

    sing up e js done

---
+++ js/login.js
@@ -0,0 +1,39 @@
+const loginButtonEl = document.getElementById('login-button');
+
+loginButtonEl.addEventListener('click', e => {
+  // console.log(loginButtonEl);
+  const usernameInput = document.getElementById('username');
+  const passwordInput = document.getElementById('password');
+
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  // first empty check korte hbe ..
+  if (!username) {
+    alert('Please enter your username');
+    usernameInput.focus();
+    return;
+  }
+  if (!password) {
+    alert('Please enter your password');
+    passwordInput.focus();
+    return;
+  }
+
+  // login korbe jodi ei sotto true hoi tahole..otherwise else return korbe..
+  if (username === 'admin' && password === 'admin123') {
+    console.log('kausar');
+    window.location.assign('../home.html');
+  } else {
+    alert('Invalid username or password. Try again.');
+
+    passwordInput.focus();
+  }
+});
+
+// Enter press korleo login hobe
+document.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    loginButtonEl.click();
+  }
+});
